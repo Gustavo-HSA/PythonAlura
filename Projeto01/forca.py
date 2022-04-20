c8a65bc83ce2aeb896f4556dd8b6797952b31eb3
Gustavo <xdrules1995@gmail.com>
2022-04-20 14:42:04 -0300

    forca update

--- Projeto01/forca.py
+++ Projeto01/forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca***!!")
     print("**********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    # enquanto enforcou and acertou forem True o jogo vai ser executado
+    while not enforcou and not acertou:
+        chute = input("Qual a letra? ")
+        chute = chute.strip()  # strip remove espaços em branco na esquerda e direita
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index += 1
+        print("Jogando....")
+
+
+
     print("Fim do jogo")
 
 
